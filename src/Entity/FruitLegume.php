acda12edee99c9efbd615da3f12351e972e882c0
Ismael Hacquin <90777738+IsmaelHK1@users.noreply.github.com>
2022-10-07 10:18:42 +0200

    :books: make:entity

---
+++ src/Entity/FruitLegume.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FruitLegumeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FruitLegumeRepository::class)]
+class FruitLegume
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $Name = null;
+
+    #[ORM\Column]
+    private ?int $Glucides = null;
+
+    #[ORM\Column]
+    private ?int $Proteines = null;
+
+    #[ORM\Column]
+    private ?int $Lipides = null;
+
+    #[ORM\Column]
+    private ?int $Calories = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $Categories = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getGlucides(): ?int
+    {
+        return $this->Glucides;
+    }
+
+    public function setGlucides(int $Glucides): self
+    {
+        $this->Glucides = $Glucides;
+
+        return $this;
+    }
+
+    public function getProteines(): ?int
+    {
+        return $this->Proteines;
+    }
+
+    public function setProteines(int $Proteines): self
+    {
+        $this->Proteines = $Proteines;
+
+        return $this;
+    }
+
+    public function getLipides(): ?int
+    {
+        return $this->Lipides;
+    }
+
+    public function setLipides(int $Lipides): self
+    {
+        $this->Lipides = $Lipides;
+
+        return $this;
+    }
+
+    public function getCalories(): ?int
+    {
+        return $this->Calories;
+    }
+
+    public function setCalories(int $Calories): self
+    {
+        $this->Calories = $Calories;
+
+        return $this;
+    }
+
+    public function getCategories(): ?string
+    {
+        return $this->Categories;
+    }
+
+    public function setCategories(string $Categories): self
+    {
+        $this->Categories = $Categories;
+
+        return $this;
+    }
+}
---
+++ src/Repository/FruitLegumeRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\FruitLegume;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<FruitLegume>
+ *
+ * @method FruitLegume|null find($id, $lockMode = null, $lockVersion = null)
+ * @method FruitLegume|null findOneBy(array $criteria, array $orderBy = null)
+ * @method FruitLegume[]    findAll()
+ * @method FruitLegume[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class FruitLegumeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, FruitLegume::class);
+    }
+
+    public function save(FruitLegume $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(FruitLegume $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return FruitLegume[] Returns an array of FruitLegume objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('f')
+//            ->andWhere('f.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('f.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?FruitLegume
+//    {
+//        return $this->createQueryBuilder('f')
+//            ->andWhere('f.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
